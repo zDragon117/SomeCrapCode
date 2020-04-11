cb6defe2ad34579da47e4b40f80619bbcd1497a0
Z_Dragon <thepathtosuccess1346@gmail.com>
2020-04-11 16:07:49 +0700

    no message

---
+++ Cryptography/cryptography_by_fernet_UI.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+from cryptography.fernet import Fernet
+
+window = Tk()
+window.geometry('1366x768')
+window.title("Welcome to LikeGeeks app")
+
+lblKey = Label(window, text="Key", width=20).grid(row=0)
+txtKey = Entry(window, width=150)
+txtKey.grid(row=0, column=1)
+
+lblString = Label(window, text="String", width=20).grid(row=1)
+txtString = Entry(window, width=150)
+txtString.grid(row=1, column=1)
+
+text = Text(window)
+text.grid(columnspan=2, sticky="ew")
+
+
+def encrypt():
+    key = bytes(txtKey.get(), "utf-8")
+    b_str = bytes(txtString.get(), "utf-8")
+    f = Fernet(key)
+    text.insert(END, bytes.decode(f.encrypt(b_str), "utf-8"))
+
+
+def decrypt():
+    key = bytes(txtKey.get(), "utf-8")
+    b_str = bytes(txtString.get(), "utf-8")
+    f = Fernet(key)
+    text.insert(END, bytes.decode(f.decrypt(b_str), "utf-8"))
+
+
+button_frame = Frame(window)
+button_frame.grid(columnspan=2)
+btnEncrypt = Button(button_frame, text="Encrypt", command=encrypt).grid(row=0, column=0)
+
+btnDecrypt = Button(button_frame, text="Decrypt", command=decrypt).grid(row=0, column=1)
+
+window.mainloop()
